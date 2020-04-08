7d054998ee52d06ce51b282069908e0476838481
POBrien333 <954979+POBrien333@users.noreply.github.com>
2020-04-08 15:19:00 +0200

    Update coin_flipper.py

--- coin_flipper.py
+++ coin_flipper.py
@@ -1,31 +1,30 @@
-'''
-COIN FLIP SIMULATOR
-Code that simulates flipping a single coin however many times the user decides.
-The code should record the outcomes and count the number of tails and heads.'''
+# COIN FLIP SIMULATOR
+# Code that simulates flipping a single coin however many times the user decides.
+# The code should record the outcomes and count the number of tails and heads.
 
 import random
-
 headntail = ['heads','tails']
+heads = 0
+tails = 0
 
 # Coin Flip function (include counter)
-class Coins:
-    def __init__(self):
-        self.coincount=[]
+def coin_flipper():
+    random.choice(headntail)
 
-    # Coin flip function
-    def coin_flipper():
-        print(random.choice(headntail))
-
-    # Input from user asking if they want to flip a coin
+# Input from user asking if they want to flip a coin
 while True:
+
     answer = input("Do you want to flip a coin? Enter 'y' or 'n' ")
-    if answer[0].lower()=='y':
+    if answer[0].lower() == 'y':
         flip = coin_flipper()
-        coincount += flip
-        print("You got: ", flip )
+        if flip == 'heads':
+            heads += 1
+            print("You got HEADS")
+        else:
+            tails += 1
+            print("You got TAILS")
 
     else:
         print("Thanks for flipping!")
-        print("A total of ", headntail.coincount('heads'), 'heads was counted')
-        print("A total of ", headntail.coincount('tails'), 'tails was counted')
+        print("A total of ", str(heads), 'heads and', str(tails), "tails was counted")
         break
